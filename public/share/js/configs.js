bf26569541dfef78bc13ea371bf19d5e0774122c
Hramezan <hramezan@gmail.com>
2022-04-06 15:53:39 +0430

    commit no 8

--- public/share/js/configs.js
+++ public/share/js/configs.js
@@ -1,11 +1,6 @@
-try {
-    if(typeof require !== 'undefined') {
-        fingerprint = require('../../../config/which.config').fingerprint;
-    }
-} catch (e){
-    console.error('which.server load error 1',e);
-}
 
+
+var fingerprint = '<YOUR FINGERPRINT>'
 var domainUrl = 'https://<YOUR DOMAIN>';
 var brokerUrl = 'mqtts://<YOUR DOMAIN>:3008'; //secure https or wss
 var domainName = 'Blocklychain';

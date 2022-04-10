cb5905052bc8a082509e914e46113d1cf9f1abe2
hamid654 <hamid@blocklychain.io>
2022-04-10 16:10:06 +0000

    commit no 13

--- public/share/js/configs.js
+++ public/share/js/configs.js
@@ -6,8 +6,7 @@ var brokerUrl = 'mqtts://<YOUR DOMAIN>:3008'; //secure https or wss
 var domainName = 'Blocklychain';
 var developmentMode = false;
 
-
-    exports.domainUrl = domainUrl;
-    exports.domainName = domainName;
-    exports.fingerprint = fingerprint;
-    exports.brokerUrl = brokerUrl;
+exports.domainUrl = domainUrl;
+exports.domainName = domainName;
+exports.fingerprint = fingerprint;
+exports.brokerUrl = brokerUrl;

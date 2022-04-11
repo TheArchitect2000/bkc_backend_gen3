d7716841cf3395a5d878bffab7117f009390a821
hamid654 <hamid@blocklychain.io>
2022-04-11 06:56:13 +0000

    commit no 14.1

--- public/share/js/configs.js
+++ public/share/js/configs.js
@@ -1,8 +1,12 @@
-var fingerprint = '<YOUR FINGERPRINT>'
-var domainUrl = 'https://<YOUR DOMAIN>';
-var brokerUrl = 'mqtts://<YOUR DOMAIN>:3008'; //secure https or wss
-var domainName = 'Blocklychain';
+
+var fingerprint = 'FB:63:86:B4:94:13:15:77:5D:BE:6A:FE:68:61:FB:E2:D8:AF:E1:F0';
+var domainUrl = 'https://panel.cpvanda.com';
+var brokerUrl = 'mqtts://panel.cpvanda.com:3008'; //secure https or wss
+var domainName = 'cpvanda';
 var developmentMode = false;
+
+
+
 
 exports.domainUrl = domainUrl;
 exports.domainName = domainName;

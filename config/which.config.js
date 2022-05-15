1bf66a356b787d30f94928485629aa3d2c8cd6cf
hramezan <45670221+hramezan@users.noreply.github.com>
2022-05-16 00:00:28 +0430

    Update which.config.js

--- config/which.config.js
+++ config/which.config.js
@@ -1,17 +1,4 @@
-// This file is used only in <Project Root>/iabroker.js
 // Don't add this file to git
-
-module.exports.whichserver = 'bkcnode';
-module.exports.debugmongoose = false;
-
-// These lines are for token distribution in /controllers/iaactivity.controller.js
-// module.exports.BonusRuntime = "1 1 * * *"; // cron, 1:01AM every day, the bkc node admin distributes BKC tokens between his users
-// module.exports.BonusRuntimezone = "America/Vancouver"; // cron, time zone
-
-
-//change finger print after update certificate
-module.exports.fingerprint = '7D:D8:3C:76:6F:D8:42:F5:D2:05:6E:94:B5:14:68:EC:B6:FD:59:97';
-
 
 let brokerconfig = {};
 
@@ -19,7 +6,16 @@ brokerconfig = {
         SECURE_KEY : 'config/webprivate.pem',
         SECURE_CERT : 'config/webpublic.pem',
         SECURE_KEY20 : 'config/iabroker.certificate.key',
-        SECURE_CERT20 : 'config/iabroker.certificate.crt'
+        SECURE_CERT20 : 'config/iabroker.certificate.crt',
+
+        // These lines are for token distribution in /controllers/iaactivity.controller.js
+        BonusRuntime : "55 05 * * *", // cron, 05:55AM every day, the bkc node admin distributes BKC tokens between his users
+        BonusRuntimezone : "America/New_York", // cron, time zone
+        // Change your finger print
+        //fingerprint : '41:DC:04:1A:BC:2B:27:04:AE:F6:0B:DC:48:14:77:A3:59:B9:2B:B5',
+        fingerprint : '<YOUR FINGERPRINT>',
+        whichserver : 'bkcnode',
+        debugmongoose : false
 };
 
 // change this true if you are going to enable Alex or Google Assitant for your IoT devices.

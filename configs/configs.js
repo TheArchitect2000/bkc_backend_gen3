df9544715342e0eed838e7a5fbc21282f561d717
hramezan <45670221+hramezan@users.noreply.github.com>
2022-06-06 23:55:10 +0430

    Add files via upload

---
+++ configs/configs.js
@@ -0,0 +1,13 @@
+
+
+var domainUrl = 'https://cl.cpvanda.com';
+var brokerUrl = 'mqtts://cl.cpvanda.com:3008'; //secure https or wss
+var domainName = 'clcpvanda';
+var developmentMode = false;
+
+
+
+
+exports.domainUrl = domainUrl;
+exports.domainName = domainName;
+exports.brokerUrl = brokerUrl;

dd8113822cd9b37ada99dc5724a91010df0ce428
hamid654 <hamid@blocklychain.io>
2022-06-22 08:47:52 +0000

    Commit No: 14

--- configs/configs.js
+++
@@ -1,8 +0,0 @@
-var domainUrl = 'https://programming.cpvanda.com';
-var brokerUrl = 'mqtts://programming.cpvanda.com:3008'; //secure https or wss
-var domainName = 'programmingcpvanda';
-var developmentMode = false;
-
-exports.domainUrl = domainUrl;
-exports.domainName = domainName;
-exports.brokerUrl = brokerUrl;
